eb0aac48ab113d844949097c76a9147f2b7ee782
huajieli <lhj@08110202>
2021-10-17 22:30:48 +0800

    查询数据（即文档）

---
+++ src/main/java/com/huajieli/es/test/ESTest_Doc_Search.java
@@ -0,0 +1,40 @@
+package com.huajieli.es.test;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+/**
+ * @author huajieli
+ * @create 2021-10-17 22:24
+ *
+ * 文档查询
+ */
+public class ESTest_Doc_Search {
+    public static void main(String[] args) throws IOException {
+        //创建客户端对象
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",9200,"http")));
+
+        //1.创建请求对象
+        GetRequest request = new GetRequest().index("user").id("1001");
+        //2.客户端发送请求，获取响应对象
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        //3.打印结果信息
+        System.out.println("_index:" + response.getIndex());
+        System.out.println("_type:" + response.getType());
+        System.out.println("_id:" + response.getId());
+        System.out.println("source:" + response.getSourceAsString());
+
+        //关闭连接
+        client.close();
+    }
+}
