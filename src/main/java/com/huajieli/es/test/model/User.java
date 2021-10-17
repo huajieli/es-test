89f04eaaca9c3eb15148387c4a96f4c99585ba19
huajieli <lhj@08110202>
2021-10-17 22:22:56 +0800

    插入数据（即文档）

---
+++ src/main/java/com/huajieli/es/test/model/User.java
@@ -0,0 +1,37 @@
+package com.huajieli.es.test.model;
+
+/**
+ * @author huajieli
+ * @create 2021-10-17 22:17
+ *
+ * 新增实体
+ */
+public class User {
+    private String name;
+    private Integer age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
